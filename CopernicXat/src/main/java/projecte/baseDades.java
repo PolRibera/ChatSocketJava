faaf17835c4bf8513f9246dc3dba668e423dc89c
polri <polri@DESKTOP-RK6FUN6>
2023-10-04 20:13:49 +0200

    RF09:  “Transmissió d’un fitxer”

--- CopernicXat/src/main/java/projecte/baseDades.java
+++ CopernicXat/src/main/java/projecte/baseDades.java
@@ -13,10 +13,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  *
